4c0ee0cdd631981a704fc7eca43f22a9fcf2b7e5
Lucas Zenerato <lucas_zenerato@hotmail.com>
2020-11-17 15:45:31 -0300

    styled ssr

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,7 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
@@ -26,5 +26,33 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  } 
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <title> Lucas Sacomani Zenerato </title>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name='application-name' content='PWA App' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='PWA App' />
+          <meta name='description' content='Best PWA App in the world' />
+                    
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/pwa.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/pwa.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/pwa.png' />
+          <meta name="theme-color" content="#000"/>
+          <link rel='manifest' href='/manifest.json' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
+
+export default MyDocument
