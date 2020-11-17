2c3872b999bb34a96948e762af636693a572a152
Lucas Zenerato <lucas_zenerato@hotmail.com>
2020-11-17 14:57:05 -0300

    code improovments

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Container } from './styles';
 
 const Error404 = () => (
-  <Container>
+  <>
     <div id="error-notfound">
       <p>
         <span style={{ color: "white" }} >404 Not Founds</span>
       </p>
     </div>
-  </Container>
+  </>
 );
 
 export default Error404;
